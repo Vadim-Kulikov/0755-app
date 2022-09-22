2fd499be302eb4c6651cc73ced6f03df9685f239
Vadim-Kulikov <111572982+Vadim-Kulikov@users.noreply.github.com>
2022-09-22 10:07:35 +0300

    3.0

--- src/state.jsx
+++ src/state.jsx
@@ -2,7 +2,7 @@ const user = {
   name: "Иван",
   lastname: "Иванов",
   email: "ivan@ya.ru",
-  id: "1",
+  id: "2",
   about: "Тут я рассказываю историю своей жизни и о своих увлечениях...",
   avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
 }
